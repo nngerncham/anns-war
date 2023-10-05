5aee5f5f3241df3931baedc38ca793ad165f9d95
Nawat Ngerncham <n.ngerncham@gmail.com>
2023-10-06 01:19:23 +0700

    removed submodules!!

--- src/test_suite.rs
+++
@@ -1,9 +0,0 @@
-use std::time::Duration;
-
-#[allow(dead_code)]
-struct TestResult {
-    memory_usage: usize,
-    index_construction_time: Duration,
-    search_time: Duration,
-    recall: f64,
-}
