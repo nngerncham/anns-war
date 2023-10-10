d88a55df82e6304ade9421eed86ad3d3c9c40de7
Nawat Ngerncham <n.ngerncham@gmail.com>
2023-10-10 23:36:08 +0700

    search test

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from testbed.test_diskann import build_diskann
-from testbed.test_faiss import build_faiss
-from testbed.test_hnswlib import build_hnswlib
+from testbed.test_diskann import test_diskann
+from testbed.test_faiss import test_faiss
+from testbed.test_hnswlib import test_hnsw
+from testbed import average
+import json
 
-from sys import stderr
+REPS = 10
 
 if __name__ == '__main__':
-    print("=== testing DiskANN ===", file=stderr)
-    build_diskann()
+    total_results = {}
+    print("=== testing HNSW ===")
+    hnsw_results = [test_hnsw() for _ in range(REPS)]
+    hnsw_avg = average(hnsw_results)
+    total_results["hnsw"] = hnsw_avg
 
-    print("=== testing FAISS ===", file=stderr)
-    build_faiss()
+    print("=== testing FAISS ===")
+    faiss_results = [test_faiss() for _ in range(REPS)]
+    faiss_avg = average(faiss_results)
+    total_results["faiss"] = faiss_avg
 
-    print("=== testing HNSW ===", file=stderr)
-    build_hnswlib()
+    print("=== testing DiskANN ===")
+    diskann_results = [test_diskann() for _ in range(REPS)]
+    diskann_avg = average(diskann_results)
+    total_results["diskann"] = diskann_avg
+
+    with open("results.json", "w") as fp:
+        json.dump(total_results, fp)
--- testbed/test_diskann.py
+++ testbed/test_diskann.py
@@ -3,7 +3,7 @@ import numpy as np
 from psutil import Process
 from os import getpid, remove
 from time import time_ns
-from testbed import load_base, NUM_THREADS
+from testbed import load_base, load_query, NUM_THREADS, K, D, evaluate, TestResult
 from pathlib import Path
 
 index_directory = "/home/nawat/muic/senior/anns-war/indices/diskann"
@@ -39,11 +39,45 @@ def build_diskann():
                                         vector_dtype=np.float32)
     end_mem = proc.memory_info().rss
 
-    print(f"== DiskANN ==\n"
-          f"Time taken: {(end_time - start_time) / 1e9 / 60} minutes\n"
-          f"Memory difference: {(end_mem - start_mem) / 1e9} GB\n"
-          f"Memory at end: {end_mem / 1e9} GB")
+    # print(f"\n== DiskANN ==\n"
+    #       f"Time taken: {(end_time - start_time) / 1e9 / 60} minutes\n"
+    #       f"Memory difference: {(end_mem - start_mem) / 1e9} GB\n"
+    #       f"Memory at end: {end_mem / 1e9} GB\n")
+    return (end_time - start_time) / 1e9 / 60, (end_mem - start_mem) / 1e9, end_mem / 1e9
+
+
+def search_diskann():
+    queries, gts = load_query()
+    index = diskannpy.StaticMemoryIndex(index_directory,
+                                        distance_metric="l2",
+                                        num_threads=NUM_THREADS,
+                                        initial_search_complexity=125,
+                                        vector_dtype=np.float32)
+
+    queries, gts = load_query()
+    start_time = time_ns()
+    res, _ = index.batch_search(queries, K, 125, NUM_THREADS)
+    end_time = time_ns()
+
+    recall = evaluate(res, gts)
+    # print(f"\n== DiskANN Search ==\n"
+    #       f"Time taken: {(end_time - start_time) / 1e9 / 60} minutes\n"
+    #       f"Recall: {recall}\n")
+    return (end_time - start_time) / 1e9 / 60, recall
+
+
+def test_diskann():
+    build_time, build_mem_diff, build_mem_final = build_diskann()
+    search_time, recall = search_diskann()
+    return TestResult(
+        build_time_minutes=build_time,
+        mem_diff_gb=build_mem_diff,
+        mem_final_gb=build_mem_final,
+        search_time_minutes=search_time,
+        recall=recall,
+    )
 
 
 if __name__ == '__main__':
     build_diskann()
+    search_diskann()
